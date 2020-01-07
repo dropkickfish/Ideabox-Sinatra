e457a9cea223237ef08149cba7253ff1abcbce3f
dropkickfish <thackrayaf@gmail.com>
2020-01-07 19:16:11 +0000

    Initial commit

---
+++ app.rb
@@ -0,0 +1,18 @@
+require './idea'
+
+class IdeaBoxApp < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+  not_found do
+    erb :error
+  end
+  get '/' do
+    erb :index, locals: {ideas: Idea.all}
+  end
+  post '/' do
+    idea = Idea.new(params['idea_title'], params['idea_description'])
+    idea.save
+    redirect '/'
+  end
+end
