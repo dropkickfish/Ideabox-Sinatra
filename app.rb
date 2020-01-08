667789c1bfa0dcba092544ead2dadbdb8e04415b
33702528 <thackrayaf@gmail.com>
2020-01-08 10:43:19 +0000

    Improved POST action to give hash from params

--- app.rb
+++ app.rb
@@ -16,8 +16,7 @@ class IdeaBoxApp < Sinatra::Base
     erb :edit, locals: {id: id, idea: idea}
   end
     post '/' do
-    idea = Idea.new(title: params['idea_title'],
-                    description: params['idea_description'])
+    idea = Idea.new(params[:idea])
     idea.save
     redirect '/'
   end
